d28b10aa8271436069fa5dbf17e9c4a61a14cc8e
Shivaraj2003 <shivarajshetty635@gmail.com>
2024-03-26 14:06:21 +0530

    Filtering based on IMDB rating feature is done

--- TableView/index.js
+++ TableView/index.js
@@ -1,16 +1,17 @@
 const page_limit = 5;
 var currentPage = 1;
-
-
+var totalPages = 1;
+var filterDropdown = document.getElementById('filterDropdown');
+var data;
+var startIndex, endIndex;
 
 const btns = document.querySelectorAll('.page-link');
+var dropdown = document.getElementById('dropdown');
+
 
 for (let i = 0; i < btns.length; i++) {
-    let curBtn = btns[i];
     btns[i].addEventListener('click', () => {
-
         let pageNum = btns[i].textContent;
-
         if (pageNum === 'Prev') {
             if (currentPage == 1)
                 // document.getElementById("prev").disabled = true;
@@ -20,13 +21,9 @@ for (let i = 0; i < btns.length; i++) {
                 currentPage -= 1;
             }
         }
-
-
-
-
         else if (pageNum == 'Next') {
-            let totalPages = Math.ceil(100 / page_limit);
             if (currentPage == totalPages) {
+                alert('done');
                 document.getElementById("next").disabled = true;
 
             }
@@ -42,29 +39,34 @@ for (let i = 0; i < btns.length; i++) {
             document.getElementById("prev").disabled = false;
 
         }
-
-
-
-        loadDataAndDisplay(currentPage);
-
+        if(currentPage > 1)
+{
+   dropdown.style.display='none';
+}
+else{
+    dropdown.style.display='block';
+}
+        renderData();
     }
     )
 }
 
-document.getElementById('prev').addEventListener('click', () => {
-
-});
-
-
 function updatePageNum(btn) {
     if (btn == 'Next') {
+        if(totalPages<=3)
+        {
+            return;
+        }
         for (let i = 1; i < btns.length - 1; i++) {
             btns[i].textContent = Number(btns[i].textContent) + 1;
         }
         console.log(currentPage);
     }
-
     else if (btn == 'Prev') {
+        if(totalPages<=3)
+        {
+            return;
+        }
         for (let i = 1; i < btns.length - 1; i++) {
             btns[i].textContent = Number(btns[i].textContent) - 1;
         }
@@ -75,19 +77,74 @@ function updatePageNum(btn) {
 async function loadDataAndDisplay(currentPage = 1) {
     try {
         const response = await fetch("movies.json");
-        const data = await response.json();
+        data = await response.json();
+        totalPages = Math.ceil(data.length/page_limit);
+
         const tableBody = document.querySelector("#tableBody");
         //data.sort((a, b) => b.imdb.rating - a.imdb.rating);
-      
         tableBody.innerHTML = "";
+        // startIndex = (currentPage - 1) * 5;
+        // endIndex = currentPage * 5 - 1;
+        // for (let i = startIndex; i <= endIndex; i++) {
+        //     {
+        //         const row = document.createElement("tr");
+        //         row.innerHTML = `
+        //         <td>${i + 1}</td>
+        //         <td>${data[i].title}</td>
+        //         <td>${data[i].imdb.rating}</td>
+        //         <td>${data[i].tomatoes.viewer.rating}</td>
+        //         <td>${(data[i].imdb.rating + data[i].tomatoes.viewer.rating).toFixed(2)}</td>
+        //         <td>${data[i].plot}</td>
+        //         `;
+        //         tableBody.appendChild(row);
+        //     }
+        // }   
+        renderData();
+    } catch (error) {
+        console.error("Error fetching or parsing data:", error);
+    }
+}
 
-        let startIndex = (currentPage - 1) * 5;
-        let endIndex = currentPage * 5 - 1;
+filterDropdown.addEventListener('click', function (e) {
+    
+    if (e.target.classList.contains('dropdown-item')) {
+        var selectedValue = e.target.getAttribute('data-value');
+        sortItems(selectedValue);
+        console.log('Selected Value:', selectedValue);
+    }
+});
 
-        for (let i = startIndex; i <= endIndex; i++) {
-            {
-                const row = document.createElement("tr");
-                row.innerHTML = `
+async function sortItems(sI) {
+    const originalData = JSON.parse(JSON.stringify(data)); 
+    data = originalData.filter(item => item.imdb.rating >= sI);
+    console.log(data);
+
+    await renderData(true);
+   //data = originalData;
+}
+
+
+function renderData(sort = false) {
+
+    const tableBody = document.querySelector("#tableBody");
+totalPages = Math.ceil(data.length/5);
+    if (sort) {
+       
+        currentPage =1;
+        startIndex = (currentPage - 1) * 5;
+        endIndex = currentPage * 5 - 1;
+    }
+    else{
+        startIndex = (currentPage - 1) * 5;
+        endIndex = currentPage * 5 - 1;
+    }
+
+    //data.sort((a, b) => b.imdb.rating - a.imdb.rating);
+    tableBody.innerHTML = "";
+    for (let i = startIndex; i <= endIndex; i++) {
+        {
+            const row = document.createElement("tr");
+            row.innerHTML = `
         <td>${i + 1}</td>
         <td>${data[i].title}</td>
         <td>${data[i].imdb.rating}</td>
@@ -95,33 +152,8 @@ async function loadDataAndDisplay(currentPage = 1) {
         <td>${(data[i].imdb.rating + data[i].tomatoes.viewer.rating).toFixed(2)}</td>
         <td>${data[i].plot}</td>
       `;
-                tableBody.appendChild(row);
-            }
+            tableBody.appendChild(row);
         }
-
-
-        //     data.forEach((movie, index) => {
-        // if(index>=startIndex && index<=endIndex)
-        //       {const row = document.createElement("tr");
-        //       row.innerHTML = `
-        //         <td>${index + 1}</td>
-        //         <td>${movie.title}</td>
-        //         <td>${movie.imdb.rating}</td>
-        //         <td>${movie.tomatoes.viewer.rating}</td>
-        //         <td>${(movie.imdb.rating + movie.tomatoes.viewer.rating).toFixed(2)}</td>
-        //         <td>${movie.plot}</td>
-        //       `;
-        //       tableBody.appendChild(row);}
-
-        //     });
-
-    } catch (error) {
-        console.error("Error fetching or parsing data:", error);
     }
-
-    
 }
-
-
-
 loadDataAndDisplay();
